dec1ff22af75de5929ca2aa3bf98dfa90e76254c
JaeXploit <81491448+JaeXploit@users.noreply.github.com>
2021-12-03 00:18:42 +0700

    ddos.py

--- hammer.py
+++ hammer.py
@@ -75,7 +75,7 @@ def dos2():
 
 
 def usage():
-	print (''' \033[92m	Hammer Dos Script v.1 http://www.canyalcin.com/
+	print (''' \033[92m	Hammer Ddos Script v.1 http://www.canyalcin.com/
 	It is the end user's responsibility to obey all applicable laws.
 	It is just for server testing script. Your ip is visible. \n
 	usage : python3 hammer.py [-s] [-p] [-t]
