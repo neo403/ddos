778ff2fb91e9512f824f62f3a1c73107b33a39e4
Kepo luh <81491448+JaeXploit@users.noreply.github.com>
2021-12-02 17:05:19 +0700

    hammer.py

--- hammer.py
+++ hammer.py
@@ -79,7 +79,8 @@ def usage():
 	It is the end user's responsibility to obey all applicable laws.
 	It is just for server testing script. Your ip is visible. \n
 	usage : python3 hammer.py [-s] [-p] [-t]
-	-h : help
+	Hacked By Jaexploit
+        -h : help
 	-s : server ip
 	-p : port default 80
 	-t : turbo default 135 \033[0m''')
